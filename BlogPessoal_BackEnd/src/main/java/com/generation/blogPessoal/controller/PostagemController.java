61c5acca1b7a6b5375c12b0e70126d38a17c5221
ji hye <jihye83@hotmail.com>
2022-10-03 12:20:34 -0300

    getMapping, putMapping, postMapping, deleteMapping

--- BlogPessoal_BackEnd/src/main/java/com/generation/blogPessoal/controller/PostagemController.java
+++ BlogPessoal_BackEnd/src/main/java/com/generation/blogPessoal/controller/PostagemController.java
@@ -1,33 +1,86 @@
 package com.generation.blogPessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogPessoal.model.Postagem;
 import com.generation.blogPessoal.repository.PostagemRepository;
 
 @RestController
-@RequestMapping("/postagem")
+@RequestMapping("/postagens")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
 
 	@Autowired
 	private PostagemRepository postagemRepository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll(){
+	public ResponseEntity<List<Postagem>> getAll() {
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
+
+	/*
+	 * consultando informação pelo metodo ID. .map mapiando e trazendo OK .orElse se
+	 * nao trazaer resposta do erro
+	 */
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 	
+	// Consultando pelo titulo. getByTirulo = cria o nome para meu metodo
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	// Cadastrando uma postagem. @RequestBody requicao de corpo do model
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	// Atualizando ou Editando postagem
+	@PutMapping
+	public ResponseEntity<Postagem> put (@Valid @RequestBody Postagem postagem){
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+				.body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+		
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		postagemRepository.deleteById(id);
+	}
 }
 /*
  * @CrossOrigin indica que a classe controladora permitirá o recebimento de
  * requisições realizadas de fora do domínio (localhost e futuramente do heroku
  * quando o deploy for realizado) ao qual ela pertence
+ * 
  * @Autowired (Injeção de Dependência)
  */
--- BlogPessoal_BackEnd/src/main/java/com/generation/blogPessoal/repository/PostagemRepository.java
+++ BlogPessoal_BackEnd/src/main/java/com/generation/blogPessoal/repository/PostagemRepository.java
@@ -1,12 +1,17 @@
 package com.generation.blogPessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogPessoal.model.Postagem;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-
+	//criando um metodo, consultando pelo titulo
+	//SELECT * FROM tb_postagens WHERE titulo LIKE "%titulo%";
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
 //Long representa a nossa PrimaryKey do ID.
