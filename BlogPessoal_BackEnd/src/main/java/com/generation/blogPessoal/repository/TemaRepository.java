d5f41b79ca6038e210030c01b45c282cd01aa0ea
ji hye <jihye83@hotmail.com>
2022-10-04 16:06:44 -0300

    adicionando @Param

--- BlogPessoal_BackEnd/src/main/java/com/generation/blogPessoal/repository/TemaRepository.java
+++ BlogPessoal_BackEnd/src/main/java/com/generation/blogPessoal/repository/TemaRepository.java
@@ -3,6 +3,7 @@ package com.generation.blogPessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogPessoal.model.Tema;
@@ -10,6 +11,6 @@ import com.generation.blogPessoal.model.Tema;
 @Repository
 public interface TemaRepository extends JpaRepository<Tema, Long>{
 
-	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("tema") String descricao);
 	
 }
